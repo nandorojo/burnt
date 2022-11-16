c0467b13343aacc831ffa90c7e3467cadd690ead
Fernando Rojo <frojo@sas.upenn.edu>
2022-11-16 15:47:29 -0500

    Update BurntModule.swift

--- ios/BurntModule.swift
+++ ios/BurntModule.swift
@@ -151,13 +151,7 @@ enum ToastPreset: String, Enumerable {
 }
 
 public class BurntModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('Burnt')` in JavaScript.
     Name("Burnt")
 
     AsyncFunction("toastAsync") { (options: ToastOptions) -> Void in
