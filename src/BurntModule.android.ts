5576241b354c05e6bde337a260cf08b2bff699cb
Fernando Rojo <frojo@sas.upenn.edu>
2022-12-04 15:47:13 -0500

    android

--- src/BurntModule.android.ts
+++ src/BurntModule.android.ts
@@ -2,10 +2,16 @@ import { AlertOptions, ToastOptions } from "./types";
 import { ToastAndroid } from "react-native";
 
 export default {
-  alertAsync(_: AlertOptions) {
+  alertAsync({ message, title, preset, duration }: AlertOptions) {
     console.warn(
-      "[burnt] Burnt.alertAsync() is not implemented on this Android. Please try toastAsync()."
+      "[burnt] Burnt.alert() is not implemented on this Android. Falling back to toast()."
     );
+    this.toastAsync({
+      message,
+      title,
+      preset: preset == "done" ? "done" : preset == "error" ? "error" : "done",
+      duration,
+    });
   },
   toastAsync({ title, duration = 5 }: ToastOptions) {
     ToastAndroid.showWithGravityAndOffset(
