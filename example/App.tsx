ab8fa54f8b20f510b660a387de422e4eefb45ab4
Herrick Fang <herrickfang@gmail.com>
2023-12-05 13:37:23 -0800

    Fix svg warnings

--- example/App.tsx
+++ example/App.tsx
@@ -224,9 +224,9 @@ const WebIcon = () => {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     >
       <path d='m14 12-8.5 8.5a2.12 2.12 0 1 1-3-3L11 9'></path>
       <path d='M15 13 9 7l4-4 6 6h3a8 8 0 0 1-7 7z'></path>
